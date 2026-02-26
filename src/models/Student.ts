b502e9ed6388e65a3a74bc3c760bb0a20d78f2c4
Andres Guzman <cesarandresguzman27@gmail.com>
2026-02-25 22:31:29 -0500

    Se modifico y agrego codigo a los controladores y studiante

---
+++ src/models/Student.ts
@@ -0,0 +1,35 @@
+import { Model, DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+class Student extends Model {
+    public id!: number;
+    public name!: string;
+    public email!: string;
+    public password!: string;
+}
+
+Student.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+}, {
+    sequelize,
+    modelName: 'Student',
+});
+
+export default Student;
